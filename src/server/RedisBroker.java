5e3cfb8fcf37ba3922fb6ecc64a163957c781268
Karan Patel <karan8patel@gmail.com>
2020-12-06 00:51:24 -0500

    added request and response for broker

--- src/server/RedisBroker.java
+++ src/server/RedisBroker.java
@@ -3,9 +3,6 @@ package server;
 import io.lettuce.core.RedisClient;
 import io.lettuce.core.RedisURI;
 import io.lettuce.core.api.StatefulRedisConnection;
-import io.lettuce.core.api.async.RedisAsyncCommands;
-import io.lettuce.core.api.sync.RedisKeyCommands;
-import io.lettuce.core.api.sync.RedisStringCommands;
 import org.zeromq.*;
 
 import java.util.LinkedList;
@@ -13,10 +10,13 @@ import java.util.Queue;
 
 public class RedisBroker extends Thread{
     private final int SERVER_TASKS = 1;
-    private final int REDIS_WORKERS = 2;
+    private final int REDIS_WORKERS = 10;
 
     private String redisHost;
     private int redisPort;
+
+    private ServerTask serverTask;
+    private Queue<String> workerQueue;
 
     public RedisBroker(String host, int port){
         this.redisHost = host;
@@ -37,17 +37,17 @@ public class RedisBroker extends Thread{
 
             for (int i = 0; i < REDIS_WORKERS; i++) {
                 new RedisConnectorWorker(redisHost, redisPort).start();
-                new RedisConnectorWorker(redisHost, redisPort + 2).start();
-                new RedisConnectorWorker(redisHost, redisPort + 4).start();
             }
 
-            for (int i = 0; i < SERVER_TASKS; i++) new ServerTask().start();
+            serverTask = new ServerTask();
+            serverTask.start();
 
-            Queue<String> workerQueue = new LinkedList<String>();
+            workerQueue = new LinkedList<String>();
+
+            ZMQ.Poller items = context.createPoller(2);
+            items.register(redisSocket, ZMQ.Poller.POLLIN);
 
             while (!Thread.currentThread().isInterrupted()) {
-                ZMQ.Poller items = context.createPoller(2);
-                items.register(redisSocket, ZMQ.Poller.POLLIN);
 
                 if (workerQueue.size() > 0)
                     items.register(serverSocket, ZMQ.Poller.POLLIN);
@@ -60,8 +60,33 @@ public class RedisBroker extends Thread{
                     //  Second frame is empty
                     String empty = redisSocket.recvStr();
                 }
+
+                if (items.pollin(1)) {
+                    String reqMsg = workerQueue.poll();
+                    redisSocket.send(reqMsg);
+                }
             }
         }
+    }
+
+    public String getToken(int port, String key){
+        String reqMsg = port + ",get," + key;
+        return serverTask.request(reqMsg);
+    }
+
+    public String setToken(int port, String key, String val){
+        String reqMsg = port + ",set," + key + "," + val;
+        return serverTask.request(reqMsg);
+    }
+
+    public String getKeys(int port, String pattern){
+        String reqMsg = port + ",keys," + pattern;
+        return serverTask.request(reqMsg);
+    }
+
+    public String delToken(int port, String key){
+        String reqMsg = port + ",del," + key;
+        return serverTask.request(reqMsg);
     }
 
 
@@ -93,6 +118,11 @@ public class RedisBroker extends Thread{
             }
         }
 
+        public String request(String reqMsg){
+            client.send(reqMsg);
+            return client.recvStr();
+        }
+
         public void closeServerConn(){
             client.close();
             context.close();
@@ -107,41 +137,54 @@ public class RedisBroker extends Thread{
         private RedisClient redisClientKeys, redisClientRequest, redisClientResponse;
         private StatefulRedisConnection<String, String> connection;
 
-        private RedisAsyncCommands<String, String> async;
-        private RedisKeyCommands<String, String> syncKeyCmd;
-        private RedisStringCommands<String, String> syncStringCmd;
-
         int portNum;
 
         public RedisConnectorWorker(String host, int portNum){
             this.portNum = portNum;
-            RedisURI redisURIKeys = RedisURI.Builder.redis(host, portNum).build();
-            this.redisClientKeys = RedisClient.create(redisURIKeys);
+            RedisURI redisURI = RedisURI.Builder.redis(host, portNum).build();
+            this.redisClientKeys = RedisClient.create(redisURI);
 
-            RedisURI redisURIRequest = RedisURI.Builder.redis(host, portNum + 2).build();
-            this.redisClientRequest = RedisClient.create(redisURIRequest);
+            redisURI = RedisURI.Builder.redis(host, portNum + 2).build();
+            this.redisClientRequest = RedisClient.create(redisURI);
 
-            RedisURI redisURIResponse = RedisURI.Builder.redis(host, portNum + 4).build();
-            this.redisClientResponse = RedisClient.create(redisURIResponse);
+            redisURI = RedisURI.Builder.redis(host, portNum + 4).build();
+            this.redisClientResponse = RedisClient.create(redisURI);
         }
 
         @Override
         public void run(){
             startConnection();
+
             while (!Thread.currentThread().isInterrupted()){
                 String[] msg = client.recvStr().split(",");
+                switch (Integer.valueOf(msg[0])){
+                    case(6379):
+                        this.connection = redisClientKeys.connect();
+                        break;
+                    case(6381):
+                        this.connection = redisClientRequest.connect();
+                        break;
+                    case(6383):
+                        this.connection = redisClientResponse.connect();
+                        break;
+                    default:
+                        break;
+                }
+
                 String rsp = "";
-                if(msg[0].equals("get")) {
-                    rsp = connection.sync().get(msg[1]);
-                } else if(msg[0].equals("set")){
-                    rsp = connection.sync().set(msg[1], msg[2]);
-                } else if(msg[0].equals("keys")){
-                    rsp = connection.sync().keys(msg[1]).toString();
-                } else if(msg[0].equals("del")){
-                    rsp = connection.sync().del(msg[1]).toString();
+                if(msg[1].equals("get")) {
+                    //rsp = connection.sync().get(msg[2]);
+                    System.out.println("get requested");
+                } else if(msg[1].equals("set")){
+                    //rsp = connection.sync().set(msg[2], msg[3]);
+                } else if(msg[1].equals("keys")){
+                    //rsp = connection.sync().keys(msg[2]).toString();
+                } else if(msg[1].equals("del")){
+                    //rsp = connection.sync().del(msg[2]).toString();
                 }
 
                 client.send(rsp);
+                connection.close();
             }
             closeConnection();
         }
@@ -165,8 +208,9 @@ public class RedisBroker extends Thread{
 
 
         public void closeConnection(){
-            connection.close();
-            //redisClient.shutdown();
+            redisClientKeys.shutdown();
+            redisClientRequest.shutdown();
+            redisClientResponse.shutdown();
 
             //zmq
             client.close();
